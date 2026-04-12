42951922ed0fafd1fb338852e211586a23e71436
itmawaran4-rgb <itmawaran4@gmail.com>
2026-04-12 11:03:14 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
  */
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbw0Y7vbx4Z-XljBlHym7Ngywcrw56eDva7Rg94lIAMeFpX1pzdylYcYThoxxAO3jPnb/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwkLO3DZ8QufZpT-fTiVmTHd5AsV3UuWEhgFSptATA1U2hkAmkUKaY4wmI37uyRzBBF/exec',
   APP_NAME: 'HR Nexus',
   SESSION_KEY: 'hr_nexus_session',
   VERSION: '1.0.0'
